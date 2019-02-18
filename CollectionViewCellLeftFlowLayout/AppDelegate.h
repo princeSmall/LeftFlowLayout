2204df58bbd81443ff298e71560eaed78f667cd3
le tong <1263512924@qq.com>
2019-02-18 14:00:42 +0800

    Initial Commit

---
+++ CollectionViewCellLeftFlowLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewCellLeftFlowLayout
+//
+//  Created by le tong on 2019/2/18.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewCellLeftFlowLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewCellLeftFlowLayout
+//
+//  Created by le tong on 2019/2/18.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
